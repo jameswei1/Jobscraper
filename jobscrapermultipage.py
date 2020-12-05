6bfa58985c5b1e3dbc92bf51b72b2b6fe36d7a6e
James Wei <jameswei27@gmail.com>
2020-12-05 14:14:13 -0500

    Removed unnecessary prints and strings

--- jobscrapermultipage.py
+++ jobscrapermultipage.py
@@ -1,5 +1,4 @@
 #works for all pages of one url
-
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup 
 import gc
@@ -8,23 +7,16 @@ import tracemalloc
 # tracemalloc.start() #------Memory stuff
 
 def scrapepage(pageurl, f):
+	#Go to provided url
 	uClient = uReq(pageurl)
 	page_html = uClient.read()
 	uClient.close()
 
+	#Scrape page and find containers for job postings
 	page_soup = soup(page_html, "html.parser") 
-
 	containers = page_soup.findAll("div", {"data-tn-component":"organicJob"})
 
 	for x in containers:
-		#Unnecessary prints for debugging
-		print("Company-Name: " + x.div.div.span.text.strip())
-		print("Job-Title: " + x.a.text.strip())
-		print("Location: " + x.find("span", {"class":"location"}).text)
-		link = 'https://ca.indeed.com' + x.a.get('href')
-		print("Link-To-Apply: " + link)
-		print("-------------------------------------------------")
-
 		f.write(
 			x.div.div.span.text.strip().replace(",", "") 
 			+ "," + x.a.text.strip().replace(",", "")
@@ -33,16 +25,14 @@ def scrapepage(pageurl, f):
 			+ "\n"
 		)
 
+	#Determine current page, next page
 	curr_page = page_soup.find("b", {"aria-current":"true"}).text
 	next_page = int(curr_page) + 1
 
+	#If next page exists, recursively scrape it
 	if (page_soup.find("a", {"aria-label":next_page})):
 		scrapepage("https://ca.indeed.com" + page_soup.find("a", {"aria-label":next_page}).get('href'), f)
 
-#------------------------------------------------------------------------------------------end of function
-
-#Sets seed URL
-myURL1 = 'https://ca.indeed.com/jobs?q=software+developer+co-op&l=Canada'
 
 #Creates CSV file
 filename = "jobs.csv"
@@ -56,9 +46,7 @@ scrapepage('https://ca.indeed.com/jobs?q=software+developer+co-op&l=Canada', f)
 f.close()
 
 # Memory stuff
-
 # gc.collect()
-
 # current, peak = tracemalloc.get_traced_memory()
 # print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 # tracemalloc.stop()
