48af9adcc06744dc98501e372bce1e90ba33028f
James Wei <jameswei27@gmail.com>
2021-01-07 12:20:11 -0500

    Added function for glassdoor, not done, compiles

--- jobscrapermultipage.py
+++ jobscrapermultipage.py
@@ -1,12 +1,11 @@
 #works for all pages of one url
-from urllib.request import urlopen as uReq
+from urllib.request import urlopen as uReq, Request
 from bs4 import BeautifulSoup as soup 
 import gc
 import tracemalloc
-
 # tracemalloc.start() #------Memory stuff
 
-def scrapepage(pageurl, f):
+def scrapepageindeed(pageurl, f):
 	#Go to provided url
 	uClient = uReq(pageurl)
 	page_html = uClient.read()
@@ -31,18 +30,37 @@ def scrapepage(pageurl, f):
 
 	#If next page exists, recursively scrape it
 	if (page_soup.find("a", {"aria-label":next_page})):
-		scrapepage("https://ca.indeed.com" + page_soup.find("a", {"aria-label":next_page}).get('href'), f)
+		scrapepageindeed("https://ca.indeed.com" + page_soup.find("a", {"aria-label":next_page}).get('href'), f)
 
-#Creates CSV file
-filename = "jobs.csv"
-f = open(filename, "w")
+def scrapepageglassdoor(pageurl, g):
+	#Go to provided url
+	req = Request(pageurl, headers = {'User-Agent': 'Mozilla/5.0'})
+	uClient = uReq(req)
+	page_html = uClient.read()
+	uClient.close()
+
+	#Scrape page and find containers for job postings
+	page_soup = soup(page_html, "html.parser")
+	
+
+#Creates indeed CSV file
+filenameindeed = "indeedjobs.csv"
+f = open(filenameindeed, "w")
 headers = "Company-Name, Job-Title, Location, Link-To-Apply\n"
 f.write(headers)
 
+#Creates glassdoor CSV file
+filenameglassdoor = "glassdoorjobs.csv"
+g = open(filenameglassdoor, "w")
+headers = "Company-Name, Job-Title, Location, Link-To-Apply\n"
+g.write(headers)
+
 #Scraoes seed url and next pages
-scrapepage('https://ca.indeed.com/jobs?q=software+developer+co-op&l=Canada', f)
+#scrapepageindeed('https://ca.indeed.com/jobs?q=software+developer+co-op&l=Canada', f)
+scrapepageglassdoor('https://www.glassdoor.ca/Job/software-developer-co-op-jobs-SRCH_KO0,24.htm', g)
 
 f.close()
+g.close()
 
 # Memory stuff
 # gc.collect()
